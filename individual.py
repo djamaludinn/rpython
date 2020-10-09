daecc75fa9d31a862d6a5a1ea1f33f1517a82b79
djamaludinn <kotgrom23@gmail.com>
2020-10-09 20:59:50 +0300

    individual.py

---
+++ individual.py
@@ -0,0 +1,7 @@
+import math
+a = int(input("Введите первое число"))
+b = int(input("Введите второе число"))
+x1 = (a+b)/2
+x2 = math.sqrt(a*b)
+print("Среднее арифметическое: {0} . ".format(x1))
+print("Среднее геометрическое: {0} . ".format(x2))
