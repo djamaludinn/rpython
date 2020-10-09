b3e1859b660f7ee38f19687a5ce1d16f721fcf0e
djamaludinn <kotgrom23@gmail.com>
2020-10-09 20:55:18 +0300

    user.py

---
+++ user.py
@@ -0,0 +1,6 @@
+userName = input("What is your name?")
+old = input("How old are you?")
+index = input("Where are you live?")
+print (("This is") + userName)
+print (("It is") + old)
+print (("He live in") + index)
